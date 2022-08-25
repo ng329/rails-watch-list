8acef9cb442426e8c19f9d7c821fac00891695c4
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-25 15:55:23 +0100

    style first index list page

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,5 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
-import NavbarController from "./navbar_controller"
+import NavbarController from "./navbar_controller.js"
 application.register("navbar", NavbarController)
--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,5 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
+    console.log("hello")
+  }
+  updateNavbar() {
+    console.log("hello")
+    if (window.scrollY >= window.innerHeight) {
+      this.element.classList.add("navbar-lewagon-white")
+    } else {
+      this.element.classList.remove("navbar-lewagon-white")
+    }
   }
 }
