b0f84d7b6ccad989594eabcf8178f668545c44cb
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-25 12:04:38 +0100

    added create list feature

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,10 +9,25 @@ class ListsController < ApplicationController
   end
 
   def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
   def set_list
-    @list = List.find(params[:list_id])
+    @list = List.find(params[:id])
   end
 end
