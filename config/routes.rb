e5a58a64bcab995d25eb52d625691c86fead3fc2
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-25 12:17:05 +0100

    add bookmark controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[index new create destroy]
+  end
 end
