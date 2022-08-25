e82e1f1e50ca254b0c449e78961f92c3e13fe84a
Natasha Gill <natashamgill@hotmail.co.uk>
2022-08-25 12:34:59 +0100

    add controller routes & methods for bookmark

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,31 @@
 class BookmarksController < ApplicationController
+  before_action :set_list
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_bookmarks_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
 end
