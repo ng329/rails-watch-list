c4e3a4821a8edc9f70a23ad04a4daf05a81638a9
Natasha Gill <natashamgill@hotmail.co.uk>
2023-02-23 17:02:05 +0000

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+puts 'clearing database'
 Bookmark.delete_all
 List.delete_all
 Movie.delete_all
+puts 'finished clearing database'
 
+puts 'adding movies'
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+# user_serialized = URI.open(url).read
+
+(1..20).each do |n|
+  movies_serialized = URI.open("#{url}?page=#{n}").read
+  movies = JSON.parse(movies_serialized)['results']
+  movies.each do |movie|
+    title = movie['title']
+    overview = movie['overview']
+    poster_url = "https://image.tmdb.org/t/p/original#{movie['poster_path']}"
+    rating = movie['vote_average']
+    Movie.create(title: title, overview: overview, poster_url: poster_url, rating: rating)
+  end
+  puts "#{n}"
+end
+
+# List.create(name: "sci-fi", photo: "")
+# List.create(name: "fantasy", photo: "")
+# List.create(name: "Drama", photo: "https://media.glamour.com/photos/5ec2e91dccfbc8c1a8fe8cbf/master/w_3000,h_2032,c_limit/MSDTITA_FE057.jpg")
